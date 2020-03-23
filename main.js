4c7b8e296b27d5fa81cab173d4dd899b0a2c3511
Rich Boakes <rjb@port.ac.uk>
2020-03-23 16:48:39 +0000

    cleanup

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ import { BankAcc } from "./bankacc.mjs";
 
 const ba = new BankAcc( "Damien", 666, 100000, 23 );
 
-console.log(ba.balance); // 23
 report(ba);
+
 ba.deposit(45);
-console.log(ba.balance); // 68
 report(ba);
+
 ba.withdraw(10);
-console.log(ba.balance); // 58
 report(ba);
 
 function report(account) {
